010b177624fb4bb640575c005cf44d9c0055530e
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2026-01-19 12:48:17 +0100

    build: add core dependencies and DI setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+
 }
 
 android {
@@ -40,7 +43,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +51,15 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.play.services.location)
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.work.runtime.ktx)
+    ksp(libs.androidx.room.compiler)
+    ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/io/github/rebeccacalabretta/teamops/ui/TeamOpsApp.kt
@@ -0,0 +1,8 @@
+package io.github.rebeccacalabretta.teamops
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import kotlin.text.Typography.dagger
+
+@HiltAndroidApp
+class TeamOpsApp : Application()
