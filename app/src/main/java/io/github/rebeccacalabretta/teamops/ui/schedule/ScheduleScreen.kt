2af69dd54ad2ede3502c360d206eace270055dbf
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2026-02-16 17:35:47 +0100

    feat: create ScheduleScreen foundation

--- app/src/main/java/io/github/rebeccacalabretta/teamops/ui/schedule/ScheduleScreen.kt
+++ app/src/main/java/io/github/rebeccacalabretta/teamops/ui/schedule/ScheduleScreen.kt
@@ -4,21 +4,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScheduleScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Arbeitsplan")
+
+    Column(modifier = modifier.fillMaxSize()) {
+
+        TopAppBar(
+            title = { Text("Arbeitsplan") }
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+        }
     }
 }
