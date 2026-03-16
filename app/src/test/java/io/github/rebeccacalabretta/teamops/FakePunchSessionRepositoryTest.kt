522b08636c983856f679424d1c1f79ffe2ff5dbf
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2026-03-16 16:26:12 +0100

    test: add checkOut unit test for FakePunchSessionRepository

--- app/src/test/java/io/github/rebeccacalabretta/teamops/FakePunchSessionRepositoryTest.kt
+++ app/src/test/java/io/github/rebeccacalabretta/teamops/FakePunchSessionRepositoryTest.kt
@@ -1,7 +1,10 @@
 package io.github.rebeccacalabretta.teamops
 
+import android.location.Location
+import io.github.rebeccacalabretta.teamops.data.db.ObjectEntity
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertNull
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
@@ -21,5 +24,36 @@ class FakePunchSessionRepositoryTest {
 
         assertNotNull(openSession)
         assertEquals("emp1", openSession?.employeeId)
+    }
+
+    @Test
+    fun checkOut_closes_open_session() = runTest {
+        val repository = FakePunchSessionRepository()
+
+        repository.checkIn(
+            objectId = "obj1",
+            employeeId = "emp1",
+            currentUserId = "user1"
+        )
+
+        val location = Location("test")
+
+        val objectEntity = ObjectEntity(
+            id = "obj1",
+            name = "Test Object",
+            latitude = 0.0,
+            longitude = 0.0,
+            radiusMeters = 50
+        )
+
+        repository.checkOut(
+            endLocation = location,
+            objectEntity = objectEntity,
+            currentUserId = "user1"
+        )
+
+        val openSession = repository.getOpenSessionOrNull()
+
+        assertNull(openSession)
     }
 }
