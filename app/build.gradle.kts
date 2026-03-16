873f2d50ab4d624dbf09d74c60ee091c0da1240b
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2026-03-16 15:37:36 +0100

    chore: setup unit and UI testing infrastructure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     ksp(libs.androidx.room.compiler)
     ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
