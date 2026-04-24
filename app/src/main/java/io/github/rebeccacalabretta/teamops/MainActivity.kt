785045e09f3aa5a1d8774c8f652d1db83b2fedb8
RebeccaCalabretta <rebecca.calabretta@gmail.com>
2026-04-24 22:26:28 +0200

    feat: add splash screen and improve app start

--- app/src/main/java/io/github/rebeccacalabretta/teamops/MainActivity.kt
+++ app/src/main/java/io/github/rebeccacalabretta/teamops/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.rebeccacalabretta.teamops.ui.AppStart
 import io.github.rebeccacalabretta.teamops.ui.theme.TeamOpsTheme
@@ -11,6 +12,8 @@ import io.github.rebeccacalabretta.teamops.ui.theme.TeamOpsTheme
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
--- app/src/main/java/io/github/rebeccacalabretta/teamops/ui/AppStart.kt
+++ app/src/main/java/io/github/rebeccacalabretta/teamops/ui/AppStart.kt
@@ -1,8 +1,13 @@
 package io.github.rebeccacalabretta.teamops.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.github.rebeccacalabretta.teamops.viewmodel.AuthStateViewModel
@@ -20,6 +25,12 @@ fun AppStart() {
         }
 
         userId.isEmpty() -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
         }
 
         else -> {
